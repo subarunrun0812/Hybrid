76fb13468edd60481efd59c752029426c8f5116d
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-16 10:04:08 +0900

    exitDoorを抜けた後の部屋を作成した

--- Assets/script/SilentScript.cs
+++ Assets/script/SilentScript.cs
@@ -4,46 +4,48 @@ using UnityEngine;
 
 public class SilentScript : MonoBehaviour
 {
-    [SerializeField] private AudioSource silentBGM;
-    [SerializeField] private AudioSource timerBGM;
-    [SerializeField] private SilentTimer silentTimer;
-    [SerializeField] private SphereCollider enemyChasingDetector;
-    private GameObject[] silentsArray;
-    private GameObject[] leadLampArray;
+	[SerializeField] private AudioSource silentBGM;
+	[SerializeField] private AudioSource timerBGM;
+	[SerializeField] private SilentTimer silentTimer;
+	[SerializeField] private SphereCollider enemyChasingDetector;
+	private GameObject[] silentsArray;
+	private GameObject[] leadLampArray;
+	[SerializeField] private MazeExitDoor mazeExitDoor;
 
-    private void Start()
-    {
-        silentsArray = GameObject.FindGameObjectsWithTag("Silent");
-        leadLampArray = GameObject.FindGameObjectsWithTag("LeadLamp");
-        silentTimer.enabled = false;
-    }
-    public void PlaySilentBGM()
-    {
-        silentBGM.Play();
-    }
+	private void Start()
+	{
+		silentsArray = GameObject.FindGameObjectsWithTag("Silent");
+		leadLampArray = GameObject.FindGameObjectsWithTag("LeadLamp");
+		silentTimer.enabled = false;
+	}
+	public void PlaySilentBGM()
+	{
+		silentBGM.Play();
+	}
 
-    public void StopSilentBGM()
-    {
-        silentBGM.Stop();
-    }
+	public void StopSilentBGM()
+	{
+		silentBGM.Stop();
+	}
 
-    public void StartSilent()//サイレンのライトと音楽を作動させる。爆発する前に逃げるシーン
-    {
-        Debug.Log("StartSilentが呼ばれた");
-        PlaySilentBGM();
-        timerBGM.Play();
-        enemyChasingDetector.radius = 30;//playerをずっと追いかけるようにする
-        silentTimer.enabled = true;
+	public void StartSilent()//サイレンのライトと音楽を作動させる。爆発する前に逃げるシーン
+	{
+		Debug.Log("StartSilentが呼ばれた");
+		PlaySilentBGM();
+		timerBGM.Play();
+		enemyChasingDetector.radius = 30;//playerをずっと追いかけるようにする
+		silentTimer.enabled = true;
+		mazeExitDoor.ExitDoorOpneFlagTrue();//ExitDoorを開けれるようにする
 
-        for (int i = 0; i < silentsArray.Length; i++)//すべてのサイレントをスタートさせる
-        {
-            BlinkingLamp blinkingLamp = silentsArray[i].GetComponent<BlinkingLamp>();
-            blinkingLamp.StartBlinkingLamp();
-        }
-        for (int i = 0; i < leadLampArray.Length; i++)//すべてのサイレントをスタートさせる
-        {
-            LeadLampScript leadLampScript = leadLampArray[i].GetComponent<LeadLampScript>();
-            leadLampScript.LeadLampFunction();
-        }
-    }
+		for (int i = 0; i < silentsArray.Length; i++)//すべてのサイレントをスタートさせる
+		{
+			BlinkingLamp blinkingLamp = silentsArray[i].GetComponent<BlinkingLamp>();
+			blinkingLamp.StartBlinkingLamp();
+		}
+		for (int i = 0; i < leadLampArray.Length; i++)//すべてのサイレントをスタートさせる
+		{
+			LeadLampScript leadLampScript = leadLampArray[i].GetComponent<LeadLampScript>();
+			leadLampScript.LeadLampFunction();
+		}
+	}
 }
