3dd81e80c5b47fc164bcba0147cddf43884f1ac3
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-17 18:51:51 +0900

    componentを整理した

--- Assets/MazeExitDoor.cs
+++ Assets/MazeExitDoor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using
+using UnityEngine.Playables;
 
 [RequireComponent(typeof(AudioSource))]
 public class MazeExitDoor : MonoBehaviour
@@ -10,12 +10,15 @@ public class MazeExitDoor : MonoBehaviour
     private AudioSource audioSource;
     public bool OpenFlag;
     [SerializeField] private AudioClip notOpenSE;
+    [SerializeField] private GameObject timeline;//timelineexplosionをアタッチする
+    [SerializeField] private GameObject enemy;//普通のenemy
 
     void Start()
     {
         //doorのロックがされている時 false
         OpenFlag = false;
         audioSource = this.gameObject.GetComponent<AudioSource>();
+        timeline.SetActive(false);
     }
     public void ExitDoorOpneFlagTrue()//サイレンが呼ばれた時にtrueになる
     {
@@ -30,9 +33,16 @@ public class MazeExitDoor : MonoBehaviour
         }
         else//ドアが開けれる時,//timelintのカットシーンに入る
         {
-
+            StartTimeLine();
         }
     }
+    private void StartTimeLine()//timelintを再生する
+    {
+        timeline.SetActive(true);
+        enemy.SetActive(false);
+    }
+
+
     private void NotOpenAnim()//鍵がかかっている時ドアをガチャガチャ
     {
         DOTween.Sequence()
@@ -42,6 +52,5 @@ public class MazeExitDoor : MonoBehaviour
         .Append(this.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.095f))
         .Append(this.transform.DOLocalRotate(new Vector3(0, 2, 0), 0.095f))
         .Append(this.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.095f));
-
     }
 }
