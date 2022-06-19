5f7e0ba862bfe8becc91805561517ce74b8ff9a8
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-20 08:41:24 +0900

    signalsのEndExplosionを追加した

---
+++ Assets/script/SignalMazeExitDoor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalMazeExitDoor : MonoBehaviour
+{
+    public void Enter_ExitDoor()//
+    {
+
+    }
+    public void EndExplosion()
+    {
+        Debug.Log("EndExplosionが呼び出された");
+    }
+}
