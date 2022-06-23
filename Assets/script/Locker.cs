4b5f5110045959aa8fbb7804bbe68a53119f0f72
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-23 16:41:52 +0900

    enemyの足音の音量を大きくした

--- Assets/script/Locker.cs
+++ Assets/script/Locker.cs
@@ -6,58 +6,58 @@ using DG.Tweening;
 [RequireComponent(typeof(AudioSource))]
 public class Locker : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private Crouch crouch;
-    [SerializeField] private GameObject lockerParent;
-    private AudioSource audioSource;
-    [SerializeField] private AudioClip openSE;
-    [SerializeField] private AudioClip closeSE;
-    [SerializeField] private GameObject cube;
-    public float openTime;
-    public float closeTime;
-    private bool inLocker = false;//lockerの中に入っていたらtrue
+	[SerializeField] private GameObject player;
+	[SerializeField] private Crouch crouch;
+	[SerializeField] private GameObject lockerParent;
+	private AudioSource audioSource;
+	[SerializeField] private AudioClip openSE;
+	[SerializeField] private AudioClip closeSE;
+	[SerializeField] private GameObject cube;
+	public float openTime;
+	public float closeTime;
+	private bool inLocker = false;//lockerの中に入っていたらtrue
 
-    void Start()
-    {
-        audioSource = this.gameObject.GetComponent<AudioSource>();
-    }
+	void Start()
+	{
+		audioSource = this.gameObject.GetComponent<AudioSource>();
+	}
 
-    public void GoInsideLocker()
-    {
+	public void GoInsideLocker()
+	{
 
-        if (inLocker == false)//ロッカーに入っていないとき。
-        {
-            this.transform.DOLocalRotate(new Vector3(0, -60, 0), openTime);
-            audioSource.PlayOneShot(openSE);
+		if (inLocker == false)//ロッカーに入っていないとき。
+		{
+			this.transform.DOLocalRotate(new Vector3(0, -60, 0), openTime);
+			audioSource.PlayOneShot(openSE);
 
-            //ロッカーの隙間から外を見るため、カメラの高さを高くする
-            crouch.headToLower.localPosition = new Vector3
-       (crouch.headToLower.localPosition.x, crouch.crouchYHeadPosition = 1.75f, crouch.headToLower.localPosition.z);
+			//ロッカーの隙間から外を見るため、カメラの高さを高くする
+			crouch.headToLower.localPosition = new Vector3
+	   (crouch.headToLower.localPosition.x, crouch.crouchYHeadPosition = 1.75f, crouch.headToLower.localPosition.z);
 
-            player.transform.DOMove((lockerParent.transform.position), openTime).OnComplete(() =>
-             {
-                 this.transform.DOLocalRotate(new Vector3(0, 0, 0), closeTime);
-                 audioSource.PlayOneShot(closeSE);
-             });
+			player.transform.DOMove((lockerParent.transform.position), openTime).OnComplete(() =>
+			 {
+				 this.transform.DOLocalRotate(new Vector3(0, 0, 0), closeTime);
+				 audioSource.PlayOneShot(closeSE);
+			 });
 
 
 
-            inLocker = true;
-        }
-        else//ロッカーに入っているとき
-        {
-            this.transform.DOLocalRotate(new Vector3(0, -120, 0), openTime);
-            audioSource.PlayOneShot(openSE);
-            //ロッカーの隙間から外を見るため、カメラの高さを高くする
-            crouch.headToLower.localPosition = new Vector3
-           (crouch.headToLower.localPosition.x, crouch.crouchYHeadPosition = 1.4f, crouch.headToLower.localPosition.z);
+			inLocker = true;
+		}
+		else//ロッカーに入っているとき
+		{
+			this.transform.DOLocalRotate(new Vector3(0, -120, 0), openTime);
+			audioSource.PlayOneShot(openSE);
+			//ロッカーの隙間から外を見るため、カメラの高さを高くする
+			crouch.headToLower.localPosition = new Vector3
+		   (crouch.headToLower.localPosition.x, crouch.crouchYHeadPosition = 1.4f, crouch.headToLower.localPosition.z);
 
-            player.transform.DOMove((cube.transform.position), openTime).OnComplete(() =>
-             {
-                 this.transform.DOLocalRotate(new Vector3(0, 0, 0), closeTime);
-                 audioSource.PlayOneShot(closeSE);
-             });
-            inLocker = false;
-        }
-    }
+			player.transform.DOMove((cube.transform.position), openTime).OnComplete(() =>
+			 {
+				 this.transform.DOLocalRotate(new Vector3(0, 0, 0), closeTime);
+				 audioSource.PlayOneShot(closeSE);
+			 });
+			inLocker = false;
+		}
+	}
 }
--- Assets/script/caricatureTrap.cs
+++ Assets/script/caricatureTrap.cs
@@ -5,94 +5,94 @@ using DG.Tweening;
 using UnityEngine.AI;
 public class caricatureTrap : MonoBehaviour
 {
-    [SerializeField] private GameObject enemy;
-    [SerializeField] private int time;
-    [SerializeField] private GameObject enemyWatiSpot;//絵の後ろについてるenemy待機の場所にあるobj
-    [SerializeField] private int picPosY;//絵の待機~登場時のy
-    [SerializeField] private EnemyController enemyController;
-    [SerializeField] private Material eye_m;//fukuroの目のmaterial
-    private Animator animator;//enemyのanimator
-    private NavMeshAgent enemyNavmesh;
-    private GameObject[] trapsArray;//enemyが追いかけている時はtrapは発動しないようにする
-    private List<Collider> trapsList = new List<Collider>();
-    void Start()//enemy以外のオブジェクトからenemyの表示を制御する
-    {
-        enemy.SetActive(false);
-        enemyNavmesh = enemy.GetComponent<NavMeshAgent>();
-        animator = enemy.GetComponent<Animator>();
-        eye_m.color = Color.black;
-        //全てのtrapのcollider取得
-        trapsArray = GameObject.FindGameObjectsWithTag("Trap");
-        foreach (var item in trapsArray)
-        {
-            trapsList.Add(item.GetComponent<Collider>());
-        }
-    }
-    void OnTriggerEnter(Collider col)
-    {
-        if ((col.transform.gameObject.tag == "Player") || (col.transform.gameObject.tag == "BackRoomsDoor") || (col.transform.gameObject.tag == "NormalDoor"))
-        {
-            EntryEnemy();
-        }
-    }
-    public void EntryEnemy()
-    {
-        Debug.Log("トラップが作動した");
-        //enemyの威嚇animationからリセット
-        // animator.SetTrigger("ResetTrigger");
-        enemy.transform.position = new Vector3(
-            enemyWatiSpot.transform.position.x,
-            picPosY,
-            enemyWatiSpot.transform.position.z
-        );
-        //一度通ったトラップn秒発動した
-        this.gameObject.GetComponent<Collider>().enabled = false;
-        StartCoroutine("HideTrapCorutine");
+	[SerializeField] private GameObject enemy;
+	[SerializeField] private int time;
+	[SerializeField] private GameObject enemyWatiSpot;//絵の後ろについてるenemy待機の場所にあるobj
+	[SerializeField] private int picPosY;//絵の待機~登場時のy
+	[SerializeField] private EnemyController enemyController;
+	[SerializeField] private Material eye_m;//fukuroの目のmaterial
+	private Animator animator;//enemyのanimator
+	private NavMeshAgent enemyNavmesh;
+	private GameObject[] trapsArray;//enemyが追いかけている時はtrapは発動しないようにする
+	private List<Collider> trapsList = new List<Collider>();
+	void Start()//enemy以外のオブジェクトからenemyの表示を制御する
+	{
+		enemy.SetActive(false);
+		enemyNavmesh = enemy.GetComponent<NavMeshAgent>();
+		animator = enemy.GetComponent<Animator>();
+		eye_m.color = Color.black;
+		//全てのtrapのcollider取得
+		trapsArray = GameObject.FindGameObjectsWithTag("Trap");
+		foreach (var item in trapsArray)
+		{
+			trapsList.Add(item.GetComponent<Collider>());
+		}
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if ((col.transform.gameObject.tag == "Player") || (col.transform.gameObject.tag == "BackRoomsDoor") || (col.transform.gameObject.tag == "NormalDoor"))
+		{
+			EntryEnemy();
+		}
+	}
+	public void EntryEnemy()
+	{
+		Debug.Log("トラップが作動した");
+		//enemyの威嚇animationからリセット
+		// animator.SetTrigger("ResetTrigger");
+		enemy.transform.position = new Vector3(
+			enemyWatiSpot.transform.position.x,
+			picPosY,
+			enemyWatiSpot.transform.position.z
+		);
+		//一度通ったトラップn秒発動した
+		this.gameObject.GetComponent<Collider>().enabled = false;
+		StartCoroutine("HideTrapCorutine");
 
-        enemy.SetActive(false);//威嚇のanimationを再生するため
-        enemy.SetActive(true);
-        //壁から登場する前の待機
-        enemy.transform.LookAt(this.gameObject.transform.position);//飛び出す方向に向kう
-        eye_m.color = new Color32(209, 0, 0, 1);//ほぼ赤色
+		enemy.SetActive(false);//威嚇のanimationを再生するため
+		enemy.SetActive(true);
+		//壁から登場する前の待機
+		enemy.transform.LookAt(this.gameObject.transform.position);//飛び出す方向に向kう
+		eye_m.color = new Color32(209, 0, 0, 1);//ほぼ赤色
 
-        enemy.transform.DOLocalMove(new Vector3(
-            this.transform.position.x,
-            picPosY,
-            this.transform.position.z
-            ), time).OnComplete(() =>
-        {//enemyが壁から登場しanimationをした後に追いかける
-            enemyController.EnemyAgentMove();
-        });
-    }
+		enemy.transform.DOLocalMove(new Vector3(
+			this.transform.position.x,
+			picPosY,
+			this.transform.position.z
+			), time).OnComplete(() =>
+		{//enemyが壁から登場しanimationをした後に追いかける
+			enemyController.EnemyAgentMove();
+		});
+	}
 
-    IEnumerator HideTrapCorutine()//トラップが発動するのをn秒やめる
-    {
-        yield return new WaitForSeconds(20f);
-        this.gameObject.GetComponent<Collider>().enabled = true;
-    }
-    void Update()
-    {
-        //enemyがいない場合はもう一度トラップを起動する
-        if (enemy.activeSelf == true)
-        {
-            eye_m.color = new Color32(209, 0, 0, 1);//ほぼ赤色
+	IEnumerator HideTrapCorutine()//トラップが発動するのをn秒やめる
+	{
+		yield return new WaitForSeconds(20f);
+		this.gameObject.GetComponent<Collider>().enabled = true;
+	}
+	void Update()
+	{
+		//enemyがいない場合はもう一度トラップを起動する
+		if (enemy.activeSelf == true)
+		{
+			eye_m.color = new Color32(209, 0, 0, 1);//ほぼ赤色
 
-            // enemyの瞬間移動を防ぐため
-            for (int item = 0; item < trapsList.Count; item++)
-            {
-                trapsList[item].enabled = false;
-                Debug.Log("trapsのcolliderが非表示になった");
-            }
-        }
-        else
-        {
-            eye_m.color = Color.black;
+			// enemyの瞬間移動を防ぐため
+			for (int item = 0; item < trapsList.Count; item++)
+			{
+				trapsList[item].enabled = false;
+				Debug.Log("trapsのcolliderが非表示になった");
+			}
+		}
+		else
+		{
+			eye_m.color = Color.black;
 
-            //enemyをもう一度出すことが可能になる
-            for (int item = 0; item < trapsList.Count; item++)
-            {
-                trapsList[item].enabled = true;
-            }
-        }
-    }
+			//enemyをもう一度出すことが可能になる
+			for (int item = 0; item < trapsList.Count; item++)
+			{
+				trapsList[item].enabled = true;
+			}
+		}
+	}
 }
