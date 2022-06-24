2c6d96db1c70ec9ce415e9e704f0ec1c7b708498
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-25 00:13:54 +0900

    コミット

--- Assets/script/KeyDoor_Door.cs
+++ Assets/script/KeyDoor_Door.cs
@@ -6,51 +6,51 @@ using DG.Tweening;
 [RequireComponent(typeof(AudioSource))]
 public class KeyDoor_Door : MonoBehaviour
 {
-    [Header("door,key番号")]
-    [SerializeField] private int num;
-    private AudioSource audioSource;
-    [SerializeField] private AudioClip openSE;
-    [SerializeField] private AudioClip closeSE;
-    [SerializeField] private AudioClip notOpenSE;
-    [SerializeField] private AudioClip getkeySE;
-    private bool OpenFlag;
-    [SerializeField] private KeyDoor keyDoor;
-    void Start()
-    {
-        //doorが閉まっているときはtrue
-        OpenFlag = true;
-        audioSource = this.gameObject.GetComponent<AudioSource>();
-    }
-    public void IsNearDoor()//keydoorを選択したときに一番最初によばれるかんすう 
-    {
-        keyDoor.KeyFlag(num);
-    }
-    public void OpenDoor()
-    {
-        Debug.Log("OpenFlagが呼ばれた");
-        if (OpenFlag == true)//ドアを開けるとき
-        {
-            this.transform.DOLocalRotate(new Vector3(-90, 0, -120), 1.6f);
-            OpenFlag = false;
-            audioSource.PlayOneShot(openSE);
+	[Header("door,key番号")]
+	[SerializeField] private int num;
+	private AudioSource audioSource;
+	[SerializeField] private AudioClip openSE;
+	[SerializeField] private AudioClip closeSE;
+	[SerializeField] private AudioClip notOpenSE;
+	private bool OpenFlag;
+	[SerializeField] private KeyDoor keyDoor;
+	void Start()
+	{
+		//doorが閉まっているときはtrue
+		OpenFlag = true;
+		audioSource = this.gameObject.GetComponent<AudioSource>();
+	}
+	public void IsNearDoor()//keydoorを選択したときに一番最初によばれるかんすう 
+	{
+		keyDoor.KeyFlag(num);
+	}
+	public void OpenDoor()
+	{
+		Debug.Log("OpenDoorが呼ばれた");
+		if (OpenFlag == true)//ドアを開けるとき
+		{
+			this.transform.DOLocalRotate(new Vector3(-90, 0, -120), 1.6f);
+			OpenFlag = false;
+			audioSource.PlayOneShot(openSE);
 
-        }
-        else//ドアを閉めるとき
-        {
-            this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 1.0f);
-            OpenFlag = true;
-            audioSource.PlayOneShot(closeSE);
-        }
-    }
-    public void NotOpenAnim()//鍵が閉まっているとき
-    {
-        audioSource.PlayOneShot(notOpenSE);
-        DOTween.Sequence()
-        .Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 2), 0.095f))
-        .Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 0.095f))
-        .Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 2), 0.095f))
-        .Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 0.095f))
-        .Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 2), 0.095f))
-        .Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 0.095f));
-    }
+		}
+		else//ドアを閉めるとき
+		{
+			this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 1.0f);
+			OpenFlag = true;
+			audioSource.PlayOneShot(closeSE);
+		}
+	}
+	public void NotOpenAnim()//鍵が閉まっているとき
+	{
+		Debug.Log("NotOpenAnimが呼ばれた");
+		audioSource.PlayOneShot(notOpenSE);
+		DOTween.Sequence()
+		.Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 2), 0.095f))
+		.Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 0.095f))
+		.Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 2), 0.095f))
+		.Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 0.095f))
+		.Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 2), 0.095f))
+		.Append(this.transform.DOLocalRotate(new Vector3(-90, 0, 0), 0.095f));
+	}
 }
--- Assets/script/KeyDoor_Key.cs
+++ Assets/script/KeyDoor_Key.cs
@@ -5,10 +5,11 @@ using DG.Tweening;
 
 public class KeyDoor_Key : MonoBehaviour
 {
-    [SerializeField] private KeyDoor keyDoor;
-    public void GetKey()//鍵を入手した時の処理
-    {
-        keyDoor.KeySE();//効果音を鳴らす
-        this.gameObject.SetActive(false);
-    }
+	[SerializeField] private KeyDoor keyDoor;
+	public void GetKey()//鍵を入手した時の処理
+	{
+		Debug.Log("GetKeyが呼ばれた");
+		keyDoor.KeySE();//効果音を鳴らす
+		this.gameObject.SetActive(false);
+	}
 }
