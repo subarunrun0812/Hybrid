2a142f7744bd354db53a5cc1b85d57d9adb03f04
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-25 16:14:13 +0900

    地下室を作成した

--- Assets/HintCrouching.cs
+++ Assets/HintCrouching.cs
@@ -11,7 +11,7 @@ public class HintCrouching : MonoBehaviour
         if (col.gameObject.tag == "Player")
         {
             _time += Time.deltaTime;
-            if (_time > 5f)
+            if (_time > 4f)
             {
                 keyCodeExplanation.MoveKeyCodeExplanation(40, "C", "しゃがみ");
                 this.gameObject.SetActive(false);
