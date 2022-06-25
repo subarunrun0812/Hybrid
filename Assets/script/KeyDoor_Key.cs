4424b99b52153fb1f26e695021d200010001ec90
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-25 14:11:21 +0900

    inspectorの中を整理した

--- Assets/script/KeyDoor_Key.cs
+++ Assets/script/KeyDoor_Key.cs
@@ -5,11 +5,14 @@ using DG.Tweening;
 
 public class KeyDoor_Key : MonoBehaviour
 {
-	[SerializeField] private KeyDoor keyDoor;
-	public void GetKey()//鍵を入手した時の処理
-	{
-		Debug.Log("GetKeyが呼ばれた");
-		keyDoor.KeySE();//効果音を鳴らす
-		this.gameObject.SetActive(false);
-	}
+    [SerializeField] private KeyDoor keyDoor;
+    [SerializeField] private KeyCodeExplanation keyCodeExplanation;
+
+    public void GetKey()//鍵を入手した時の処理
+    {
+        Debug.Log("GetKeyが呼ばれた");
+        keyDoor.KeySE();//効果音を鳴らす
+        keyCodeExplanation.MoveKeyCodeExplanation("tab", "鍵を入手した");
+        this.gameObject.SetActive(false);
+    }
 }
