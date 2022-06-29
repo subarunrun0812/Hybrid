687510729398e60b3ab12abe8b49dffa796b81a8
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-29 10:11:47 +0900

    入手したアイテムを説明するuiを作成中

---
+++ Assets/ImageItemExplanation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageItemExplanation : MonoBehaviour
+{
+    [SerializeField] private Image img;
+    //itemの説明
+    public void ImageItem(Image itemImg,string explanation)
+    {
+
+    }
+}
