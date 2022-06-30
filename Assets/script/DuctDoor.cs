b4027b9de822aba038867ce2bc174b01f6e09941
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-30 14:20:23 +0900

    ductを直した

--- Assets/script/DuctDoor.cs
+++ Assets/script/DuctDoor.cs
@@ -6,37 +6,38 @@ using DG.Tweening;
 [RequireComponent(typeof(AudioSource))]
 public class DuctDoor : MonoBehaviour
 {
-    private AudioSource audioSource;
-    [SerializeField] private AudioClip openSE;
-    [SerializeField] private GameObject driver;
-    private bool OpenFlag;
-    [SerializeField] private RequiredItemMessage requiredItemMessage;
-    void Start()
-    {
-        //doorが閉まっているときはtrue
-        OpenFlag = true;
-        audioSource = this.gameObject.GetComponent<AudioSource>();
-    }
+	private AudioSource audioSource;
+	[SerializeField] private AudioClip openSE;
+	[SerializeField] private GameObject driver;
+	private bool OpenFlag;
+	[SerializeField] private RequiredItemMessage requiredItemMessage;
+	void Start()
+	{
+		//doorが閉まっているときはtrue
+		OpenFlag = true;
+		audioSource = this.gameObject.GetComponent<AudioSource>();
+	}
 
-    public void IsNearDoctDoor()
-    {
-        //一度開けたら開けっ放しにする
-        //ドライバーが必要
-        if (OpenFlag == true && driver.activeSelf == false)//ドアを開けるとき
-        {
-            this.transform.DOLocalRotate(new Vector3(0, -180, 0), 1.6f);
-            OpenFlag = false;
-            audioSource.PlayOneShot(openSE);
-        }
-        else//ドアが開けれないとき
-        {
-            requiredItemMessage.RequiredMessage("ねじ回しが必要");
-        }
-        // else//ドアを閉めるとき
-        // {
-        //     this.transform.DOLocalRotate(new Vector3(0, 0, 0), 1.0f);
-        //     OpenFlag = true;
-        //     audioSource.PlayOneShot(closeSE);
-        // }
-    }
+	public void IsNearDoctDoor()
+	{
+		//一度開けたら開けっ放しにする
+		//ドライバーが必要
+		if (OpenFlag == true && driver.activeSelf == false)//ドアを開けるとき
+		{
+			this.transform.DOLocalRotate(new Vector3(0, -180, 0), 1.6f);
+			OpenFlag = false;
+			audioSource.PlayOneShot(openSE);
+			this.tag = "Untagged";
+		}
+		else//ドアが開けれないとき
+		{
+			requiredItemMessage.RequiredMessage("ねじ回しが必要");
+		}
+		// else//ドアを閉めるとき
+		// {
+		//     this.transform.DOLocalRotate(new Vector3(0, 0, 0), 1.0f);
+		//     OpenFlag = true;
+		//     audioSource.PlayOneShot(closeSE);
+		// }
+	}
 }
