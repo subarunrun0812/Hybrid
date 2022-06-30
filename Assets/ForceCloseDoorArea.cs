008aec6ce0c11bc9c0680374e822710a7894c28f
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-06-30 12:24:45 +0900

    書斎に入ったら、入ってきたドアを閉めるようにした

---
+++ Assets/ForceCloseDoorArea.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCloseDoorArea : MonoBehaviour
+{
+	[SerializeField] private GameObject door;
+	private KeyDoor_Door keyDoor_Door;//強制的に閉じるドア
+	private void Start()
+	{
+		keyDoor_Door = door.GetComponent<KeyDoor_Door>();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			keyDoor_Door.CloseDoor();
+			door.tag = "Untagged";
+		}
+	}
+}
