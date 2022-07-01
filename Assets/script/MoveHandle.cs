8bceb5a1fcd77a1e30bfcd9c6770ef033ec688bd
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-07-01 18:10:54 +0900

    elevetorを製作中

--- Assets/script/MoveHandle.cs
+++ Assets/script/MoveHandle.cs
@@ -10,12 +10,21 @@ public class MoveHandle : MonoBehaviour
     private AudioSource audioSource;
     [SerializeField] private AudioClip lowerSE;
     private float movetime = 0.8f;
+    [SerializeField] private Material elevator_cabin;
+    [SerializeField] private GameObject ele_light;
+
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        //emissionをオフ
+        elevator_cabin.DisableKeyword("_EMISSION");
+        ele_light.SetActive(false);
     }
-    public void LowerHandle()
+    public void LowerHandle()//ハンドルを回すとエレベーター
     {
+        //elevatorのライトをオンにする
+        elevator_cabin.EnableKeyword("_EMISSION");
+        ele_light.SetActive(true);
         this.transform.DOLocalRotate((_rotation), movetime);
         audioSource.PlayOneShot(lowerSE);
         this.tag = "Untagged";
