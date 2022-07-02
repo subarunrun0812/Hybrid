b404b32b97a9ebe0b32560794fa5d8432705bb6c
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-07-02 09:41:00 +0900

    elevatorの音を鳴らすfunctionは作った

--- Assets/ElevatorScript.cs
+++ Assets/ElevatorScript.cs
@@ -8,26 +8,55 @@ public class ElevatorScript : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip corpus_OpenDoorSE;
+    [SerializeField] private AudioClip cabin_OpenDoorSE;
+    [SerializeField] private AudioClip corpus_CloseDoorSE;
+    [SerializeField] private AudioClip cabin_CloseDoorSE;
     private float moveTime = 8f;//Audioの再生が止まるとき
     [SerializeField] private Collider col;
-    private void Start()
-    {
-        // DownMoveElevator();
-    }
-
-    public void ElevatorOpenDoor()
-    {
-        col.enabled = false;
-        animator.SetTrigger("Open");
-    }
-    public void ElevatorCloseDoor()
-    {
-        col.enabled = true;
-    }
     public void DownMoveElevator()
     {
         this.gameObject.transform.DOLocalMoveY(-5f, moveTime);
         audioSource.Play();
+    }
+    private void Start()
+    {
+        // DownMoveElevator();
+        // ElevatorOpenDoor();
+    }
+    public void ElevatorOpenDoor()
+    {
+        //外側の扉を初めに呼び出す
+        Elevator_Corpus_OpenDoor();
+        col.enabled = false;
+        animator.SetTrigger("Open");
+    }
+    //animationEventから呼び出す
+    //外側のドア
+    public void Elevator_Corpus_OpenDoor()
+    {
+        audioSource.PlayOneShot(corpus_OpenDoorSE);
+    }
+    //内側のドア
+    public void Elevator_Cabin_OpenDoor()
+    {
+        audioSource.PlayOneShot(cabin_OpenDoorSE);
+    }
+
+    //ドアを閉じるとき
+    public void Elevator_CloseDoor()
+    {
+        col.enabled = true;
+    }
+    //外側の扉
+    public void Elevator_Corpus_CloseDoor()
+    {
+        audioSource.PlayOneShot(corpus_CloseDoorSE);
+    }
+    //内側のドア
+    public void Elevator_Cabin_CloseDoor()
+    {
+        audioSource.PlayOneShot(cabin_CloseDoorSE);
     }
     private void Update()
     {
