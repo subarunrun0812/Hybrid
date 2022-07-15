2812200fa4bd4b55c8612635d1c53ca781986a77
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-07-15 13:49:49 +0900

    タイトルシーンに戻るボタンを追加した

--- Assets/FadeInEndTimeLineUI.cs
+++ Assets/FadeInEndTimeLineUI.cs
@@ -9,12 +9,17 @@ public class FadeInEndTimeLineUI : MonoBehaviour
 {
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private TextMeshProUGUI _text;
+    private float t_time = 0.12f;//１文字を表示する時間
+    [SerializeField] private GameObject titlebutton;//titlesceneに戻るボタン
+    [SerializeField] private CanvasGroup b_canvasGroup;
     private string contents =
-     "NewTexttetetdkfnvjoahfijhvajkfnvpo;avnasiljdfvhalkvjnasdofioauvnasjikldfhasfniovansidlfjhneaifjvoliasuhfnjklasdfh vaiolvhasdfjnasolfieuhjvniadfadsf";
+     "無事に、奴を仕留めれた。\nしかし、ここは一体なんの施設だっただろうか。\nなぜ、ここで人間とトナカイのハイブリッドを作っていたのだろうか。";
     private void Start()
     {
         canvasGroup.alpha = 0f;
+        b_canvasGroup.alpha = 0f;
         _text.text = "";
+        titlebutton.SetActive(false);
     }
     //TimeLineから呼び出す
     public void FadeInUI()
@@ -26,7 +31,19 @@ public class FadeInEndTimeLineUI : MonoBehaviour
     }
     private void EndStoryText()
     {
-        _text.DOText(contents, 15f).SetEase(Ease.Linear);
+        int contentsLength = contents.Length;
+        _text.DOText(contents, t_time * contentsLength).SetEase(Ease.Linear);
+        StartCoroutine("TransitionTitleScene");
+    }
+    private IEnumerator TransitionTitleScene()
+    {
+        int contentsLength = contents.Length;
+        yield return new WaitForSeconds(t_time * contentsLength + 6f);
+        titlebutton.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        b_canvasGroup.DOFade(1f, 2f);
+
     }
 
 }
